de58d0e73d6452b6cd83f189f62f1bd1011fbe71
11-hayaalkhateeb <115453315+11-hayaalkhateeb@users.noreply.github.com>
2022-11-03 17:10:40 +0200

    Add files via upload

---
+++ 3nov/task1.js
@@ -0,0 +1,27 @@
+ var mark=88;
+if ( mark >=0 && mark<=49 ){ 
+    document.write("fail");
+}
+else if ( mark >=50 && mark<=59 ){ 
+
+    document.write("D");
+}
+else if ( mark >=60 && mark<=69 ){ 
+
+    document.write("c");
+}
+else if ( mark >=70 && mark<=79 ){ 
+
+    document.write("B");
+}
+else if ( mark >=80 && mark<=89 ){ 
+
+    document.write("A");
+}
+else if ( mark >=90 && mark<=100 ){ 
+
+    document.write("A+");
+}
+else {
+    document.write("رقم غير صحيح");
+}
---
+++ 3nov/task2.js
@@ -0,0 +1,4 @@
+const pi = 3.14;
+let y = 4
+let x= 3*pi +10
+document.write(x)
